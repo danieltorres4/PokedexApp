c7001e8ab4f23cdbae9e2932ca48b690e4515ea2
danieltorres4 <dst990502@outlook.com>
2023-02-23 20:20:15 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Iván Sánchez Torres on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
